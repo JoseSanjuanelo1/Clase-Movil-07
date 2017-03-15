81ffe87a1871072228dfbe5c5b0f379195d448b1
Jose Sanjuanelo <j-os-pablo@hotmail.com>
2017-03-14 21:14:11 -0500

    Aplicacion final (verificar Validar)

--- app/src/main/java/com/example/soporte/holamundo/Principal.java
+++ app/src/main/java/com/example/soporte/holamundo/Principal.java
@@ -1,16 +1,21 @@
 package com.example.soporte.holamundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
 
     private EditText cajaNombre;
-    private TextView cajaMensaje;
+    private EditText cajaApellido;
+    private Intent i;
+    private Bundle b;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +23,45 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
         cajaNombre = (EditText)findViewById(R.id.txtNombre);
+        cajaApellido =(EditText)findViewById(R.id.txtapellido);
+        i = new Intent(this,Saludo.class);
+        b = new Bundle();
         //cajaMensaje = (TextView)findViewById(R.id.lblMensaje);
     }
 
 
     public void saludar(View v){
-        String aux;
-        aux = cajaNombre.getText().toString();
-        cajaMensaje.setText(getResources().getString(R.string.parte_saludo)+" "+aux);
+
+        String nombre, apellido;
+        if (validar()){
+        nombre = cajaNombre.getText().toString();
+        apellido = cajaApellido.getText().toString();
+
+            b.putString("Nombre", nombre);
+            b.putString("Apellido", apellido);
+            i.putExtras(b);
+
+            startActivity(i);
+        }
+
+        //cajaMensaje.setText(getResources().getString(R.string.parte_saludo)+" "+aux);
 
 
+    }
+
+    public boolean validar(){
+        if (cajaNombre.getText().toString().isEmpty() ){
+            //Toast.makeText(getApplicationContext(), "Digite por favor el nombre" , Toast.LENGTH_SHORT).show();
+            cajaNombre.setError(getResources().getString(R.string.error_1));
+            return false;
+        }
+
+        if (cajaApellido.getText().toString().isEmpty()){
+            //Toast.makeText(getApplicationContext(), "Digite por favor el apellido" , Toast.LENGTH_SHORT).show();
+            cajaNombre.setError(getResources().getString(R.string.error_2));
+            return false;
+        }
+        return true;
     }
 }
 
--- app/src/main/java/com/example/soporte/holamundo/Saludo.java
+++ app/src/main/java/com/example/soporte/holamundo/Saludo.java
@@ -1,13 +1,28 @@
 package com.example.soporte.holamundo;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Saludo extends AppCompatActivity {
+
+    private TextView Saludo;
+    private Bundle b;
+    private String aux, nombre, apellido;
+    private Resources res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+
+        Saludo = (TextView)findViewById(R.id.txtSaludo);
+        b = getIntent().getExtras();
+        nombre = b.getString("Nombre");
+        apellido = b.getString("Apellido");
+        res=this.getResources();
+        aux = res.getString(R.string.parte_saludo)+" "+nombre+" "+apellido+" "+res.getString(R.string.parte_saludo2);
+        Saludo.setText(aux);
     }
 }
