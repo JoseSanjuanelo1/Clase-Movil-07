136d34544dcd49fe1dc7585cfd460738556b9a97
Jose Sanjuanelo <j-os-pablo@hotmail.com>
2017-03-21 20:53:06 -0500

    Interfaz 3.0 Over

--- app/src/main/java/com/example/soporte/holamundo/Principal.java
+++ app/src/main/java/com/example/soporte/holamundo/Principal.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Space;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +17,11 @@ public class Principal extends AppCompatActivity {
 
     private EditText cajaNombre;
     private EditText cajaApellido;
+    String genero, estadoCivil="";
+    private Spinner comboGenero;
+    private RadioButton r1, r2, r3;
+    private ArrayAdapter<String> adapter;
+    private String[] opc;
     private Intent i;
     private Bundle b;
 
@@ -24,9 +33,21 @@ public class Principal extends AppCompatActivity {
 
         cajaNombre = (EditText)findViewById(R.id.txtNombre);
         cajaApellido =(EditText)findViewById(R.id.txtapellido);
+        comboGenero = (Spinner)findViewById(R.id.cmbGenero);
+        r1 = (RadioButton)findViewById(R.id.r1);
+        r2 = (RadioButton)findViewById(R.id.r2);
+        r3 = (RadioButton)findViewById(R.id.r3);
+
+       opc = this.getResources().getStringArray(R.array.generos);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,opc);
+
+        comboGenero.setAdapter(adapter);
+
         i = new Intent(this,Saludo.class);
         b = new Bundle();
         //cajaMensaje = (TextView)findViewById(R.id.lblMensaje);
+
     }
 
 
@@ -37,8 +58,16 @@ public class Principal extends AppCompatActivity {
         nombre = cajaNombre.getText().toString();
         apellido = cajaApellido.getText().toString();
 
+            genero = comboGenero.getSelectedItem().toString();
+            if(r1.isChecked()) estadoCivil = getResources().getString(R.string.soltero);
+            if(r2.isChecked()) estadoCivil = getResources().getString(R.string.casado);
+            if(r3.isChecked()) estadoCivil = getResources().getString(R.string.divorciado);
+
+
             b.putString("Nombre", nombre);
             b.putString("Apellido", apellido);
+            b.putString("Genero", genero);
+            b.putString("estadOCivil", estadoCivil);
             i.putExtras(b);
 
             startActivity(i);
@@ -47,6 +76,14 @@ public class Principal extends AppCompatActivity {
         //cajaMensaje.setText(getResources().getString(R.string.parte_saludo)+" "+aux);
 
 
+    }
+
+    public void borrar(View v){
+        cajaNombre.setText("");
+        cajaNombre.requestFocus();
+        cajaApellido.setText("");
+        comboGenero.setSelection(0);
+        r1.setChecked(true);
     }
 
     public boolean validar(){
--- app/src/main/java/com/example/soporte/holamundo/Saludo.java
+++ app/src/main/java/com/example/soporte/holamundo/Saludo.java
@@ -9,7 +9,7 @@ public class Saludo extends AppCompatActivity {
 
     private TextView Saludo;
     private Bundle b;
-    private String aux, nombre, apellido;
+    private String aux, nombre, apellido, estadoCivil, genero;
     private Resources res;
 
     @Override
@@ -21,8 +21,15 @@ public class Saludo extends AppCompatActivity {
         b = getIntent().getExtras();
         nombre = b.getString("Nombre");
         apellido = b.getString("Apellido");
+        genero = b.getString("Genero");
+        estadoCivil = b.getString("estadOCivil");
         res=this.getResources();
         aux = res.getString(R.string.parte_saludo)+" "+nombre+" "+apellido+" "+res.getString(R.string.parte_saludo2);
+        aux= aux+ "\n"+res.getString(R.string.datos);
+        aux = aux+"\n\n"+res.getString(R.string.nombre)+": "+nombre;
+        aux = aux+"\n"+res.getString(R.string.apellido)+": "+apellido;
+        aux = aux+"\n"+res.getString(R.string.genero)+": "+genero;
+        aux = aux+"\n"+res.getString(R.string.estadoCivil)+": "+estadoCivil;
         Saludo.setText(aux);
     }
 }
